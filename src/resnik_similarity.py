91288394991eb17b8c978dd15a777b6417617c64
Kotichintala Srihari Sakshith <kotichintalasriharisakshith@gmail.com>
2025-09-08 12:11:30 +0530

    Ensure WordNet and IC loaded for Resnik similarity computation

--- src/resnik_similarity.py
+++ src/resnik_similarity.py
@@ -9,12 +9,7 @@
 # Importing packages
 import nltk
 from nltk.corpus import wordnet, wordnet_ic
-from nltk.stem import WordNetLemmatizer
 import numpy as np
-import pickle
-import pprint
-import pandas as pd
-from scipy import stats
 
 try:
     wordnet.ensure_loaded()
@@ -53,39 +48,12 @@ def closest_synsets(word_1: str, word_2: str):
 
     return synset_1_shortest, synset_2_shortest, max_similarity
 
-# loading in the 6 documents from the resume
-documents = pickle.load(open('../assets/documents.p', 'rb'))
-print('The documents are:')
-pprint.pprint(documents)
 
-# Viewing the document as a Table
-documents_table = pd.DataFrame(documents)
-print('\nDocuments:')
-print(documents_table)
+# Taking User Input
+word_1 = input('Enter the first word:\t')
+word_2 = input('Enter the second word:\t')
+word_1_synset, word_2_synset, similarity = closest_synsets(word_1, word_2)
 
-# We will now find the similarity between the 6th document and every other document
-similarity_mat = np.zeros((len(documents) - 1, len(documents[0])))
-
-for column, keyword in enumerate(documents[len(documents) - 1]):
-    for row in range(len(documents) - 1):
-        similarity_mat[row][column] = closest_synsets(keyword, documents[row][column])[2]
-
-print('\nThe similarity coefficients are:\n')
-similarity = pd.DataFrame(similarity_mat, columns=documents[5])
-print(similarity.to_string())
-
-# saving the similarity coefficient matrix in text file
-results = open('../assets/resnik_similarity_matrix.txt', 'w')
-results.write(similarity.to_string())
-results.close()
-
-# We now select the highest and lowest similarity document for each word in the 6th document
-max = [0, 0, 0, 0, 4, 1]
-min = [3, 3, 2, 3, 4, 0]
-
-# document with least/maximum similarity
-document_min_similarity = stats.mode(min).mode[0]
-document_max_similarity = stats.mode(max).mode[0]
-
-print('\nDocument with Minimum Similarity to 6th document:', documents[document_min_similarity])
-print('Document with Maximum Similarity to 6th document:', documents[document_max_similarity])
+print(word_1.capitalize() + ' Definition:', word_1_synset.definition())
+print(word_2.capitalize() + ' Definition:', word_2_synset.definition())
+print('similarity:', similarity)
